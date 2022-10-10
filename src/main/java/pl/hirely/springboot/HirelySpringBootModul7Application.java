34c2680b826eec25961504813a28a6480cdc5b32
Serhii_Murenko <sermur9038@gmail.com>
2022-10-10 19:44:58 +0200

    Spring 10.10

---
+++ src/main/java/pl/hirely/springboot/HirelySpringBootModul7Application.java
@@ -0,0 +1,13 @@
+package pl.hirely.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HirelySpringBootModul7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HirelySpringBootModul7Application.class, args);
+	}
+
+}
---
+++ src/test/java/pl/hirely/springboot/HirelySpringBootModul7ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.hirely.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HirelySpringBootModul7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
